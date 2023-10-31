0cdb384e9fc4d2a12eaa383452c7f7551643cf34
Maksimov Vladislav <workmaximov@yandex.ru>
2023-10-31 16:53:33 +0100

    change fireEvent to userEvent in SummaryForm tests

--- src/pages/SummaryForm/SummaryForm.test.jsx
+++ src/pages/SummaryForm/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { SummaryForm } from "./SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 const renderComponent = () => render(<SummaryForm />);
 
@@ -17,20 +18,21 @@ test("initial conditions", () => {
   expect(submitButton).toBeDisabled();
 });
 
-test("checkbox enables sumbit button when checked and disables when unchecked", () => {
+test("checkbox enables sumbit button when checked and disables when unchecked", async () => {
   renderComponent();
+  const user = userEvent.setup();
 
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
   const submitButton = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
 
   expect(checkbox).toBeChecked();
   expect(submitButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
 
   expect(checkbox).not.toBeChecked();
   expect(submitButton).toBeDisabled();
